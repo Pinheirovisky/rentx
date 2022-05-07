a6a365e2f9db216e87e8b6c230a326b2c4b3b3d8
Vinicius Pinheiro <vinicius.pinheiro@dxc.com>
2022-05-07 12:10:21 -0300

    fix: add project

---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import CreateCourseService from "./CreateCourseService";
+
+export function createCourse(request: Request, response: Response) {
+  CreateCourseService.execute({
+    name: "NodeJs",
+    duration: 10,
+    educator: "Vini",
+  });
+
+  return response.json({
+    name: "NodeJs",
+    duration: 10,
+    educator: "Vini",
+  });
+}
