f3420cc93d01ce12cbbb913d66389f84e6b5610c
Vinicius Pinheiro <vinicius.pinheiro@dxc.com>
2023-06-13 19:16:54 -0300

    feat: add postgres connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
+
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
+
+import "./databases";
 
 const app = express();
 
