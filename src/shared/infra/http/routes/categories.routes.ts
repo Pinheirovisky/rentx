c25b4a929905fb8d486edf2caa6d01bcdff4d362
Vinicius Pinheiro <pinheiro.desenv@gmail.com>
2023-07-21 10:52:54 -0300

    feat: add list category controller test

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
-import { ListCategoriesController } from "@modules/cars/useCases/listCategory/ListCategoriesController";
+import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
