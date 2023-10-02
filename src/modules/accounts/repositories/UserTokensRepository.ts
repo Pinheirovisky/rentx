e791798fbfd873c075029b3d6fb87e216d09aa36
Vinicius Pinheiro <pinheiro.desenv@gmail.com>
2023-10-02 11:03:52 -0300

    feat: create userToke method

---
+++ src/modules/accounts/repositories/UserTokensRepository.ts
@@ -0,0 +1,30 @@
+import { Repository, getRepository } from "typeorm";
+import { ICreateUserTokenDTO } from "../dtos/ICreateUserTokenDTO";
+import { UserTokens } from "../infra/typeorm/entities/UserTokens";
+import { IUsersTokenRepository } from "./IUsersTokenRepository";
+
+class UserTokensRepository implements IUsersTokenRepository {
+  private repository: Repository<UserTokens>;
+
+  constructor() {
+    this.repository = getRepository(UserTokens);
+  }
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = this.repository.create({
+      expires_date,
+      refresh_token,
+      user_id,
+    });
+
+    await this.repository.save(userToken);
+
+    return userToken;
+  }
+}
+
+export { UserTokensRepository };
