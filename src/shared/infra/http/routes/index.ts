8596154a212d1ae4b0d19709774dd169d46abca7
Vinicius Pinheiro <pinheiro.desenv@gmail.com>
2023-10-03 09:19:11 -0300

    feat: add EtherealMailProvider

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -5,11 +5,13 @@ import { usersRoutes } from "./users.routes";
 import { authenticateRoutes } from "./authenticate.routes";
 import { carsRoutes } from "./cars.routes";
 import { rentalRoutes } from "./rental.routes";
+import { passwordRoutes } from "./password.routes";
 
 const router = Router();
 
 router.use("/cars", carsRoutes);
 router.use("/categories", categoriesRoutes);
+router.use("/password", passwordRoutes);
 router.use("/rentals", rentalRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
